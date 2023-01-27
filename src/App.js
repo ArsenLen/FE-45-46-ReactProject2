3e38ff0c29b6ad29c2b7817aa32039620a6ad38e
ArsenLen <laurie10@mail.ru>
2023-01-27 15:29:22 +0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // const [left, setLeft] = useState(0)
+  // const [right, setRight] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  })
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks)
+  }
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {clicks.left}
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      {clicks.right}
     </div>
   );
 }
 
 export default App;
+// Задача. При нажатии на кнопку left, менять свойство left на 1
+// При нажатии на right менять свойства right на 1 
+
+// Задача. При нажатии на кнопку left, увеличивать состояние left на 1
+// При нажатии на кнопку right, увелиичивать состояние right на 1
+// Feature Slices
+// left = 0, left = 0 + 1, left = 1
+// left = 1, left = 1 + 1, left = 2
+// let left = 0
+  // left = left + 1
+  // setLeft(left + 1) - говорит реакту перерисоваться
+  // left++
+  // left = left + 1
+
+  /*  После первого клика по left
+    const newClicks = {
+      left: 1 (clicks.left = 0, clicks.left + 1 = 1),
+      right: clicks.right
+    }
+    setClicks(newClicks)
+    clicks = {
+      left: 1,
+      right: 0
+    }
+    После первого клика по left
+    const newClicks = {
+      left: 1 (clicks.left = 1, clicks.left + 1 = 2),
+      right: clicks.right
+    }
+    clicks = {
+      left: 2,
+      right: 0
+    }
+    React
+    setClicks(clicks.left + 1)
+    JS
+    clicks = clicks.left + 1
+  */
+
